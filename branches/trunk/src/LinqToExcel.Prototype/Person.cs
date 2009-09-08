171c1be3556fc4dff3dd09dc70c7d9a30437befb
paulyoder@gmail.com <paulyoder@gmail.com@eb8a1d2c-adb8-11dd-8e2c-133076370c3f>
2009-09-08 22:18:44 +0000

    * Copied trunk to branches

---
+++ branches/trunk/src/LinqToExcel.Prototype/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel.Prototype
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FavoriteColor { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
